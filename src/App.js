1332449fbb58b796892fa33044ad2c5321e4e87b
acube201 <aabubak2@students.kennesaw.edu>
2021-06-10 15:05:29 -0400

    my first commit

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import { Button } from '@material-ui/core';
 import Header from "./component/header"
 import './App.css';
 
@@ -5,6 +8,13 @@ function App() {
   return (
     <div className="App">
     <Header/>
+    hello Wes 
+    < Button > Click me  </Button>
+    <List >
+    
+   <ListItem> <Button> books </Button> </ListItem>
+   <ListItem> drinks  </ListItem>
+    </List>
     </div>
   );
 }
