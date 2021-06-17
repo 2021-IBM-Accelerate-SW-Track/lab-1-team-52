cdfec62955df936d955684dccf58d95ff23f34e7
Caleb <cdaniels102@andersonuniversity.edu>
2021-06-17 18:24:22 -0400

    change

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from "./component/header"
 import './App.css';
+import TodoForm from "./component 2/TodoForm";
+
 
 function App() {
   return (
     <div className="App">
     <Header/>
+    <TodoForm />
     </div>
   );
 }
